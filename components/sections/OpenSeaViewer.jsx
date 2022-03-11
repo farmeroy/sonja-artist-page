7419dc152ad06c576baad3e8a58408b9bd311979
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-11 22:26:48 +0100

    added OpenSeaViewer

---
+++ components/sections/OpenSeaViewer.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+console.log('got this far')
+import OpenSeaDragon from 'openseadragon';
+console.log('imported the drageon')
+
+const OpenSeaViewer = (props) => {
+  const image = props.image; 
+  const [width, setWidth] = useState('800px');
+  const [height, setHeight] = useState('400px')
+
+  useEffect(() => {
+    const viewer = OpenSeaDragon({
+    id: "openseadragon1",
+    tileSources: '/mydz.dzi',
+      prefixUrl: "/openseadragon-images/",
+      showNavigationControl: false,
+      visibilityRatio: 1,
+      constrainDuringPan: true,
+    })
+    
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight)
+    
+
+  }, [])
+
+
+  return (
+    <div id='openseadragon1' style={{width: width, height: height}}></div>
+  )
+}
+
+export default OpenSeaViewer;
