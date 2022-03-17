67dcc895b53ab6e4b1d2db264c4c70fcb808f8f8
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-17 01:56:13 +0100

    added left button

---
+++ components/UI/LeftBtn.jsx
@@ -0,0 +1,8 @@
+import RoundButton from './RoundButton'
+import styles from './LeftBtn.module.css'
+
+const LeftBtn = () => {
+  return <RoundButton className={styles.LeftBtn} />
+}
+
+export default LeftBtn;
---
+++ components/UI/RoundButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as styles from './RoundButton.module.css';
+
+const RoundButton = (props) => {
+
+  return (
+    <button className={`${styles.button} ${horizontalDisplay}`}>
+    </button>
+  )
+};
+
+export default RoundButton;
+
+
