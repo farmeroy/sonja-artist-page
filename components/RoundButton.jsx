de0a64c7d7ebde59f4db81f30de2556a99ce1f82
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-17 01:55:41 +0100

    removed absolute position

---
+++ components/RoundButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as styles from './RoundButton.module.css';
+
+const RoundButton = (props) => {
+  return (
+    <button className={styles.button}>
+    </button>
+  )
+};
+
+export default RoundButton;
+
