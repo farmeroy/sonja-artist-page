227d2cc78eba591d57dae43e13f85f4066480ce0
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-18 10:28:28 +0100

    added image and functionality

---
+++ components/ImageThumbnail.jsx
@@ -0,0 +1,17 @@
+import ModalElement from "./UI/ModalElement"
+import styles from './ImageThumbnail.module.css'
+import Image from 'next/image'
+
+const ImageThumbnail = (props) => {
+  return (
+    <ModalElement onClick={props.onClick}>
+      <div className={styles.container}>
+        <div className={styles.spacer}></div>
+        <Image className={styles.image}  src="/thumbnails/blue3complete@0,1x.jpg" alt="full image" width='1488' height="324"/>
+        <div  className={styles.spacer}/>
+        </div>
+    </ModalElement>
+  )
+}
+
+export default ImageThumbnail;
