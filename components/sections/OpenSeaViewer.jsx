d24a1dc82abb638f1d1c2ecd8d7df60c5243cee7
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-19 21:58:45 +0100

    added sequence viewer, default zoom

--- components/sections/OpenSeaViewer.jsx
+++ components/sections/OpenSeaViewer.jsx
@@ -1,57 +1,64 @@
-import { useEffect, useState } from 'react'
-console.log('got this far')
-import OpenSeaDragon from 'openseadragon';
-console.log('imported the drageon')
+import { useEffect, useState } from "react";
+import OpenSeaDragon from "openseadragon";
+import BigRoundBtn from '../BigRoundBtn'
 
 const OpenSeaViewer = (props) => {
-  const image = props.image; 
-  const [width, setWidth] = useState('100vw');
-  const [height, setHeight] = useState('100vh')
+  const image = props.image;
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
 
   // All references to 'window' must be introducded in useEffect
   useEffect(() => {
+    
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+
     const viewer = OpenSeaDragon({
-    id: "openseadragon1",
-    tileSources: '/mydz.dzi',
+      id: "openseadragon1",
+      tileSources: ['/mydz.dzi', '/mydz_green.dzi'],
       prefixUrl: "/openseadragon-images/",
       showNavigationControl: false,
       visibilityRatio: 1,
       constrainDuringPan: true,
-      defaultZoomLevel: 10,
+      defaultZoomLevel: 5,
       visibilityRatio: 1,
       springStiffness: 1,
       animationTime: 20,
-    })
+      previousButton: '1',
+      previousButton: 'prevBtn',
+      nextButton: 'nextBtn',
+      navPrevNextWrap: true,
+      sequenceMode: true,
+      initialPage: 1,
+      preserveViewport: true,
+      showSequenceControl: true,
+    });
 
-    const mouseTracker = new OpenSeaDragon.MouseTracker({
-      element: viewer.canvas,
-      moveHandler: function(event) {
-        const v = viewer.viewport;
-        const mouse = event.position;
-        const panDelta = v.pointFromPixel(mouse, true);
-        v.panTo(panDelta);
-      }
-    }
-    )
-    
-    
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight)
-    
+      const mouseTracker = new OpenSeaDragon.MouseTracker({
+        element: viewer.canvas,
+        moveHandler: function (event) {
+          const v = viewer.viewport;
+          const mouse = event.position;
+          const panDelta = v.pointFromPixel(mouse, true);
+          v.panTo(panDelta);
+        },
+      });
+
+
     const resize = () => {
       setHeight(window.innerHeight);
-      setWidth(window.innerWidth)
-    }
-    
-    window.addEventListener('resize', resize)
+      setWidth(window.innerWidth);
+    };
 
-  }, [])
-  
-
+    window.addEventListener("resize", resize);
+  }, [props.drawing]);
 
   return (
-    <div id='openseadragon1' style={{width: width, height: height}}></div>
-  )
-}
+    <div id="openseadragon1" style={{ width: width, height: height }}>
+
+    </div>
+  );
+};
 
 export default OpenSeaViewer;
