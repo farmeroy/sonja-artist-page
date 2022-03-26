cbabb211ba78ab849a6dfc9a15b336e7f14a23c8
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-26 23:03:28 +0100

    refactor to render openseadragon navigator

--- components/ImageThumbnail.jsx
+++ components/ImageThumbnail.jsx
@@ -7,7 +7,8 @@ const ImageThumbnail = (props) => {
     <ModalElement onClick={props.onClick}>
       <div className={styles.container}>
         <div className={styles.spacer}></div>
-        <Image className={styles.image}  src="/thumbnails/blue3complete@0,1x.jpg" alt="full image" width='1488' height="324"/>
+        <div id='navigator' ></div>
+        {/* <Image className={styles.image}  src="/thumbnails/blue3complete@0,1x.jpg" alt="full image" width='1488' height="324"/> */}
         <div  className={styles.spacer}/>
         </div>
     </ModalElement>
