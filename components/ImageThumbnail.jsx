6dd8b6e1558b2e67f57f416fe4732bf32ea08eff
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-29 20:38:52 +0200

    refactor without using ModalElement

--- components/ImageThumbnail.jsx
+++ components/ImageThumbnail.jsx
@@ -1,17 +1,33 @@
 import ModalElement from "./UI/ModalElement"
 import styles from './ImageThumbnail.module.css'
 import Image from 'next/image'
+import { useState } from 'react'
+
+// const ImageThumbnail = (props) => {
+//   return (
+//     <ModalElement onClick={props.onClick}>
+//       <div className={styles.container}>
+//         <div className={styles.spacer}></div>
+//         <div id='navigator' ></div>
+//         {/* <Image className={styles.image}  src="/thumbnails/blue3complete@0,1x.jpg" alt="full image" width='1488' height="324"/> */}
+//         <div  className={styles.spacer}/>
+//         </div>
+//     </ModalElement>
+//   )
+// }
 
 const ImageThumbnail = (props) => {
+  
+
   return (
-    <ModalElement onClick={props.onClick}>
-      <div className={styles.container}>
-        <div className={styles.spacer}></div>
-        <div id='navigator' ></div>
-        {/* <Image className={styles.image}  src="/thumbnails/blue3complete@0,1x.jpg" alt="full image" width='1488' height="324"/> */}
-        <div  className={styles.spacer}/>
-        </div>
-    </ModalElement>
+    <>
+    <div className={styles.backdrop} />
+    <div className={styles.modal}>
+    <div className={styles.container}>
+      <div id='navigator'></div>
+    </div>
+    </div>
+    </>
   )
 }
 
