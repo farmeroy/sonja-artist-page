f674b65a54e80decebaa5101c133ecd63aa623e8
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-29 20:39:38 +0200

    commented out backdrop

--- components/UI/ModalElement.jsx
+++ components/UI/ModalElement.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom";
 import styles from "./ModalElement.module.css";
 
-const Backdrop = (props) => {
-  return <div onClick={props.onClick} className={styles.backdrop} ></div>;
-};
+// const Backdrop = (props) => {
+//   return <div onClick={props.onClick} className={styles.backdrop} ></div>;
+// };
 
 const Modal = (props) => {
   return <div className={styles.modal}>{props.children}</div>;
@@ -12,10 +12,10 @@ const Modal = (props) => {
 const ModalElement = (props) => {
   return(
       <>
-      {ReactDOM.createPortal(
-          <Backdrop onClick={props.onClick}></Backdrop>,
-          document.getElementById("backdrop-root")
-      )}
+      {/* {ReactDOM.createPortal( */}
+      {/*     <Backdrop onClick={props.onClick}></Backdrop>, */}
+      {/*     document.getElementById("backdrop-root") */}
+      {/* )} */}
       {ReactDOM.createPortal(
           <Modal onClick={props.onClick}>{props.children}</Modal>,
           document.getElementById("modal-root")
